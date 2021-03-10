d4e712c82163ce4f9be9eafa3592ddbb89e414a1
Yarema91 <yostrovskyiapi@gmail.com>
2021-03-11 00:04:16 +0200

    01 components

---
+++ react-21-22-01-/src/App.js
@@ -0,0 +1,13 @@
+import PaintingList from './components/PaintingList';
+import paintings from './paintings.json';
+
+
+export default function App() {
+  return (
+    <div>
+      
+        <PaintingList  items={paintings}/>
+      
+    </div>
+  );
+}
---
+++ react-21-22-01-/src/components/Section.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Section = () => {
+    return (
+        <div style="outline: 1px solid tomato;">
+            <h2></h2>
+             Контент
+        </div>
+
+    )
+};
+
+export default Section;
